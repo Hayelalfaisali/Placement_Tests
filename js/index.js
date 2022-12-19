c830744128b0ed6b112b038c93a508547135409c
Hayel Al-Faisali <97064734+Hayelalfaisali@users.noreply.github.com>
2022-12-20 01:39:24 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 
+       var btn = document.getElementById("btn");
+       btn.onclick = function() {
+           var password = prompt("Enter the password");
+           if (password == "Hanan") {
+               parent.location = 'quiz.html';
+           } else {
+               var msg = document.getElementById("msg");
+               msg.innerHTML = "كلمة المرور غير صحيحة";
+           }
+       }
